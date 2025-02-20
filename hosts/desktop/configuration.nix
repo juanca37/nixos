29edeee8fa3d1665e7694c2fa2dd6f9a1a9a3bca
juanca37 <juancarrascoalonso@hotmail.com>
2025-02-20 16:49:00 +0100

    feat: enabling docker daemon

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -31,7 +31,7 @@
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
+  virtualisation.docker.enable = true;
   # Enable networking
   networking.networkmanager.enable = true;
 
