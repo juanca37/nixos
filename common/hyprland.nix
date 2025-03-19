ced4ee722e5eb28b53a6fa3ce10c4f9ad22c716f
juanca37 <juancarrascoalonso@hotmail.com>
2025-03-19 14:03:47 +0100

    changing sddm theme

--- common/hyprland.nix
+++ common/hyprland.nix
@@ -27,7 +27,10 @@
 
   # Enable the X11 windowing system.
   services.xserver.enable = true;
-  services.xserver.displayManager.sddm.enable = true;
+  services.xserver.displayManager.sddm = {
+    enable = true;
+    theme = "maldives";
+  };
 
   services.blueman.enable = true;
 }
