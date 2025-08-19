45a99838b89b290155219375ca59572e2ab30f1d
juanca37 <juancarrascoalonso@hotmail.com>
2025-08-19 09:25:00 +0200

    Adding system wide macos config

--- darwin/configuration.nix
+++ darwin/configuration.nix
@@ -13,6 +13,20 @@
         home = "/Users/Juan.Carrasco-1";
     };
   nixpkgs.config.allowUnfree = true;
+  system.defaults = {
+		dock = {
+				autohide = true;
+				persistent-apps = [
+						"${pkgs.kitty}/Applications/kitty.app"
+						"${pkgs.obsidian}/Applications/Obsidian.app"
+						"/Applications/Microsoft Outlook.app"
+						"/Applications/Slack.app"
+						"${pkgs.firefox}/Applications/firefox.app"
+						];
+		};
+		NSGlobalDomain.AppleICUForce24HourTime = true;
+		NSGlobalDomain.AppleInterfaceStyle = "Dark";
+};
   system.primaryUser = "Juan.Carrasco-1";
   homebrew = {
 		enable = true;
